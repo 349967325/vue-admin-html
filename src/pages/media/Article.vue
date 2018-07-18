2aee5bdd0fc417e287bfd917f3353840eba94dcb
longsheng <000000>
2018-07-18 20:48:01 +0800

    文章

--- src/pages/media/Article.vue
+++ src/pages/media/Article.vue
@@ -5,7 +5,26 @@
         <Button type="primary">创建新任务</Button>
       </Row>
     <Row>
-      <div></div>
+      <h4>筛选及查询</h4>
+      <div class="chooseState">
+        <div class="state-block">
+          <RadioGroup v-model="state">
+            <Radio label="all">全部</Radio>
+            <Radio label="opti">优化中</Radio>
+            <Radio label="stop">停止中</Radio>
+            <Radio label="susp">暂停中</Radio>
+            <Radio label="time">定时中</Radio>
+          </RadioGroup>
+        </div>
+        <div class="input-block">
+          <Select>
+            <Option>搜索任务名称</Option>
+            <Option>搜索文章编号</Option>
+            <Option>搜索任务编号</Option>
+          </Select>
+          <Input icon="ios-search"/>
+        </div>
+      </div>
     </Row>
     <Row></Row>
     </Card>
